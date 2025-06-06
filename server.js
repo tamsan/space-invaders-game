46df40117cd18c255101731a300aea9176a59f7a
TAM_tamehiro <tamehiro@tam-tam.co.jp>
2025-06-06 22:45:25 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+javascriptconst express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 静的ファイルを配信
+app.use(express.static(__dirname));
+
+// ルートアクセス時にindex.htmlを表示
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
